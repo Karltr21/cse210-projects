cbf05e9848dd4e53e3d1c55d96ff5b7c4519422c
Karl Richards <karltrichards@gmail.com>
2024-09-20 16:19:10 -0600

    Real code

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,16 @@
 using System;
+// Karl Richards
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("What is your first name?");
+        string first ;
+        first = Console.ReadLine();
+        Console.WriteLine("What is your last name? ");
+        string last ;
+        last = Console.ReadLine();
+        Console.WriteLine($"Your name is {last}, {first} {last}.");
     }
 }
