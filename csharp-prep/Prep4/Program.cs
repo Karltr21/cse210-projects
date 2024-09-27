90f5598ccd7391d50243d540df6425b9017f7f7c
Karl Richards <karltrichards@gmail.com>
2024-09-27 15:47:15 -0600

    This is the code

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,40 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        int numbers;
+        string b;
+        List<int> numbers_list = new List<int>();
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.\n");
+       do{
+        Console.WriteLine("Write a Number:");
+        b = Console.ReadLine();
+        numbers = int.Parse(b);
+        numbers_list.Add(numbers);
+
+
+       } while(numbers != 0);
+        int count = 0;
+        int largest = int.MinValue;
+        int total = 0;
+
+       foreach (int num in numbers_list)
+       {
+            count +=1;
+            total += num;
+            if (num > largest)
+            {
+                largest = num;
+            }
+       }
+
+        float average = total/count;
+        Console.WriteLine($"The sum is: {total}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {largest}");
     }
 }
