f6203b219f67e93d561462dcdeb72b00b5398da8
Karl Richards <karltrichards@gmail.com>
2024-09-27 11:14:53 -0600

    Made the code

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magic = randomGenerator.Next(1, 100);
+        int guess;
+
+        
+
+        do {
+            Console.WriteLine("Guess a magic number: ");
+            string b = Console.ReadLine();
+            guess = int.Parse(b);
+            if (guess > magic)
+                Console.WriteLine("A little too high. Try again!\n");
+            else if (guess < magic)
+                Console.WriteLine("A little too low! Try again!\n");
+            else
+                Console.WriteLine("Good Job! You got it!");
+
+        } while (guess != magic);
     }
 }
